3fa0425d33ffbe96be6a1aa2a8fe3654abff590d
Jacky <119617200+jackie-jackie@users.noreply.github.com>
2022-12-07 12:28:46 +0100

    2022-12-06 lisp

---
+++ 2022/06/lisp/solve.lisp
@@ -0,0 +1,22 @@
+(load "../../../common/lisp/util.lisp")
+
+(defun find-marker (marker-length data)
+  (loop for i = 0 then (+ i 1 dupe)
+        for candidate = (subseq data i (+ i marker-length))
+        for dupe = (loop for c across candidate
+                         for j from 0
+                         with dupe = nil
+                         if (find c candidate :start (1+ j))
+                           do (setf dupe j)
+                         finally
+                           (return dupe)
+                           )
+        if (not dupe)
+          return (+ i marker-length)
+        )
+  )
+
+(let ((data (car (parse-input))))
+  (format t "~D~%" (find-marker 4 data))
+  (format t "~D~%" (find-marker 14 data))
+  )
