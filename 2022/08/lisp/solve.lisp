bd21a9adaa8b780745d1b7060ffebb860d3749b7
Jacky <119617200+jackie-jackie@users.noreply.github.com>
2022-12-08 18:36:33 +0100

    code dedup

--- 2022/08/lisp/solve.lisp
+++ 2022/08/lisp/solve.lisp
@@ -11,50 +11,40 @@
     )
   )
 
-(defmacro scene-from (trees direction)
-  `(map-matrix ,trees
-               ,direction
-               (lambda (row)
-                 (loop for h in row
-                       for rest = (cdr row) then (cdr rest)
-                       for view = (position-if (lambda (x) (>= x h)) rest)
-                       collect (if view (1+ view) (length rest)))
-                 ))
+(defmacro reduce-matrix4 (matrix fun red-row red-col red-elem)
+  `(loop for left in (map-matrix ,matrix :left ,fun)
+         for right in (map-matrix ,matrix :right ,fun)
+         for top in (map-matrix ,matrix :top ,fun)
+         for bottom in (map-matrix ,matrix :bottom ,fun)
+         ,red-row (loop for vl in left
+                   for vr in right
+                   for vt in top
+                   for vb in bottom
+                   ,red-col (,red-elem vl vr vt vb)))
   )
-
-
-(defmacro visible-from (trees direction)
-  `(map-matrix ,trees
-               ,direction
-               (lambda (row)
-                 (loop for h in row
-                       for v = t then (> h m)
-                       maximize h into m
-                       collect v)
-                 ))
-  )
-
-
 
 (let ((trees (parse-input :pre (lambda (line)
                                  (loop for c across line
                                        collect (digit-char-p c))))))
-  (format t "~D~%" (loop for left in (visible-from trees :left)
-                         for right in (visible-from trees :right)
-                         for top in  (visible-from trees :top)
-                         for bottom in  (visible-from trees :bottom)
-                         sum (loop for vl in left
-                                   for vr in right
-                                   for vt in top
-                                   for vb in bottom
-                                   count (or vl vr vt vb))))
-  (format t "~D~%" (loop for left in (scene-from trees :left)
-                         for right in (scene-from trees :right)
-                         for top in  (scene-from trees :top)
-                         for bottom in  (scene-from trees :bottom)
-                         maximize (loop for vl in left
-                                        for vr in right
-                                        for vt in top
-                                        for vb in bottom
-                                        maximize (* vl vr vt vb))))
+  (format t "~D~%" (reduce-matrix4 trees
+                                   (lambda (row)
+                                     (loop for h in row
+                                           for v = t then (> h m)
+                                           maximize h into m
+                                           collect v)
+                                     )
+                                   sum
+                                   count
+                                   or))
+  (format t "~D~%" (reduce-matrix4 trees
+                                   (lambda (row)
+                                     (loop for h in row
+                                           for rest = (cdr row) then (cdr rest)
+                                           for view = (position-if (lambda (x)
+                                                                     (>= x h)) rest)
+                                           collect (if view (1+ view) (length rest)))
+                                     )
+                                   maximize
+                                   maximize
+                                   *))
   )
