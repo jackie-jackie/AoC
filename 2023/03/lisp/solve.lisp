1ded8182a16372d84663e6034c68ac3ddb2bad4f
Jacky <jacky@amber-teahou.se>
2023-12-04 03:14:12 +0100

    remove complex number abuse

--- 2023/03/lisp/solve.lisp
+++ 2023/03/lisp/solve.lisp
@@ -4,10 +4,8 @@
 (defun engine-p (elem)
   (and (typep elem 'character) (char/= #\. elem)))
 
-(defun sum-numbers (grid)
-  (realpart (reduce #'+
-                    (remove-duplicates (remove-if-not #'numberp
-                                                      (grid-list grid :line-breaks t))))))
+(defun unique-part-numbers (list)
+  (mapcar #'car (remove-duplicates (remove-if-not #'numberp list :key #'car))))
 
 (defun ensure-number (char/number)
   (if (numberp char/number) char/number (digit-char-p char/number)))
@@ -18,6 +16,7 @@
   (loop for row from 0 below (array-dimension input 0)
         ;; combine digits into numbers
         do (loop for col from 0 below (array-dimension input 1)
+                 for index from (* row (array-dimension input 1))
                  for digit = (ensure-number (aref input row col))
                  for next-digit = (if (array-in-bounds-p input row (1+ col))
                                       (ensure-number (aref input row (1+ col)))
@@ -25,32 +24,30 @@
                  if digit
                  do (setf (aref input row col) digit)
                  if (and digit next-digit)
-                 do (setf (aref input row (1+ col)) (+ (* 10 digit) next-digit)))
-        ;; do some complex number abuse
-        do (loop for col from 0 below (array-dimension input 1)
-                 for index from (* row (array-dimension input 1))
-                 for number = (aref input row col)
-                 if (and (numberp number) (= 0 (imagpart number)))
-                 do (setf (aref input row col) (+ number (complex 0 index))))
+                 do (setf (aref input row (1+ col)) (+ (* 10 digit) next-digit))
+                 ;; run always
+                 do (setf (aref input row col) (cons (aref input row col) index)))
         ;; spread numbers
         do (loop for col from (1- (array-dimension input 1)) downto 1
                  for number = (aref input row col)
-                 if (and (numberp number) (numberp (aref input row (1- col))))
+                 if (and (numberp (car number))
+                         (numberp (car (aref input row (1- col)))))
                  do (setf (aref input row (1- col)) number)))
   ;; part 1
   (grid-stencil9 (lambda (self &rest neighbors)
-                   (if (or (not (numberp self)) (find-if #'engine-p neighbors))
+                   (if (or (not (numberp (car self)))
+                           (find-if #'engine-p neighbors :key #'car))
                        self
-                       0))
+                       '(0 . 0)))
                  input
                  :target-grid part-numbers)
   ;; part 2
   (grid-stencil9 (lambda (self &rest neighbors)
-                   (let ((parts (remove-duplicates (remove-if-not #'numberp neighbors))))
-                     (if (and (equal #\* self) (= 2 (length parts)))
-                         (* (realpart (first parts)) (realpart (second parts)))
+                   (let ((parts (unique-part-numbers neighbors)))
+                     (if (and (equal #\* (car self)) (= 2 (length parts)))
+                         (* (first parts) (second parts))
                          0)))
                  input
                  :target-grid gear-ratios)
-  (format t "~D~&" (sum-numbers part-numbers))
-  (format t "~D~&" (sum-numbers gear-ratios)))
+  (format t "~D~&" (reduce #'+ (unique-part-numbers (grid-list part-numbers))))
+  (format t "~D~&" (reduce #'+ (grid-list gear-ratios))))
